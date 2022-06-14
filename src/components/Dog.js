0792f69e22662eaf5dedef39aa5dab8cc77646d5
jeannie <jeanniezheng123@gmail.com>
2022-06-14 13:00:04 -0500

    completed components

---
+++ src/components/Dog.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Dog = () => {
+    return (
+        <li>
+            <h2>Dog named:</h2>
+            <span>Favorite Toy:</span>
+            <button>Add Toy:</button>
+        </li>
+    );
+}
+
+export default Dog;
---
+++ src/components/DogList.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Dog from "./Dog";
+
+export const DogList = () => {
+    return (
+        <ul>
+            <h2>Dog Count: 5</h2>
+            <Dog></Dog>
+            <Dog></Dog>
+            <Dog></Dog>
+            <Dog></Dog>
+            <Dog></Dog>
+        </ul>
+    );
+}
+
